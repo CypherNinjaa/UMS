865546f72e439d0f52350e9e9d4a0f7f5d8234ca
vikash <vikashkelly@gmail.com>
2025-07-26 18:34:02 +0530

    feat: Add Student Success Stories and Why Choose Us sections with styling

--- unitech/src/Pages/Home.jsx
+++ unitech/src/Pages/Home.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import HeroSection from "../Components/Home/HeroSection";
 import EventsCalendar from "../Components/Home/EventsCalendar";
 import NewsHighlights from "../Components/Home/NewsHighlights";
+import WhyChooseUs from "../Components/Home/WhyChooseUs";
+import StudentSuccessStories from "../Components/Home/StudentSuccessStories";
 
 const Home = () => {
 	return (
@@ -9,6 +11,8 @@ const Home = () => {
 			<HeroSection />
 			<EventsCalendar />
 			<NewsHighlights />
+			<StudentSuccessStories />
+			<WhyChooseUs />
 		</div>
 	);
 };
